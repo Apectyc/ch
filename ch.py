d42a3eaceb41e98e5520e610f078d53dcd016d60
Apectyc <85085465+Apectyc@users.noreply.github.com>
2022-02-04 17:59:27 -0500

    Initial commit

---
+++ ch.py
@@ -0,0 +1,36 @@
+from discord.ext import commands
+import discord, os, json
+from discord.ext import commands
+import datetime as datetime
+import bot
+from discord_components import (DiscordComponents, Button, ButtonStyle, Select, SelectOption)
+
+@commands.command('ch')
+@commands.has_permissions(administrator=True) #permissions
+async def ch(ctx, user : discord.Member, *, role : discord.Role):
+  if role.position > ctx.author.top_role.position: #if the role is above users top role it sends error
+    return await ctx.send('**:x: | That role is above your top role!**') 
+  if role in user.roles:
+      await user.remove_roles(role) #removes the role if user already has
+      await ctx.send(f"Removed {role} from {user.mention}")
+  else:
+      await user.add_roles(role) 
+      await user.send(
+
+          embed=discord.Embed(title='Welcome!', description=f'Welcome to **Valorant Customs** Hosts Team!', footer='Developed by Jute.#2022', color=255),
+                components=[
+
+                ])
+        
+      await ctx.send(f"Added {role} to {user.mention} and User DMed")
+      channel = bot.get_channel(934207770201309195)
+      await channel.send(
+
+          embed=discord.Embed(title='New Logs', description=f'{ctx.author} Add {user.mention} like Hosts.', footer='Developed by Jute.#2022', color=255),
+                components=[
+
+                ])
+
+
+def ch(bot):
+    bot.add_cog(ch(bot))
